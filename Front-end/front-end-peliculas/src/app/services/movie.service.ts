0343d880cb5eb89e89d4761bffdc4001069f35c7
aezzaididam1 <aezzaididam1@gmail.com>
2024-06-18 13:24:19 +0200

    añadidos metodos para actualizar puntuación y para hacer el fetch de la api a la base de datos en services

--- Front-end/front-end-peliculas/src/app/services/movie.service.ts
+++ Front-end/front-end-peliculas/src/app/services/movie.service.ts
@@ -20,4 +20,15 @@ export class MovieService {
   getMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(this.apiUrl);
   }
+
+   // Método para actualizar la puntuación de una película
+   updateMovieRating(imdbID: string, rating: number): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${imdbID}/rating/${rating}`, {});
+  }
+
+  // Método para obtener películas desde la api a la base de datos
+  fetchMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.apiUrl}/fetch`);
+  }
+
 }
